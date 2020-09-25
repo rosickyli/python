b941adddd1057713f6436ff31b73a7b10a666ada
rosickyli <32864696+rosickyli@users.noreply.github.com>
2020-09-25 14:27:52 +0800

    Update question2.py

--- question2.py
+++ question2.py
@@ -1,3 +1,20 @@
+"""
+Create an aggregate dataset `agg_skill_combo.csv` containing the number of unique players grouped by skill combinations. Dataset columns:
+
+- `skill_combo`
+- `number_of_unique_players`
+- `date_time_min` (format: 'yyyy-mm-dd hh:mm:ss')
+- `date_time_max` (format: 'yyyy-mm-dd hh:mm:ss')
+
+Requirements:
+
+- Skill combinations are expected to be **skill a + skill b**, for example **drone + hive**.
+- For players who only equip one skill then skill combo is **skill a**, for example **drone**.
+- It doesn’t matter if players equip the skill on left or right slot, we consider it is the same skill combination regardless of the slot. Which means **drone + hive** equals **hive + drone** and only one version of the combination should appear in the dataset.
+- Players who do not equip any skill are not included in the dataset.
+- `date_time_min` and `date_time_max` correspond to the date range of the aggregated dataset, hence should have a constant value for each row of the dataset.
+"""
+
 import pandas as pd
 import numpy as np
 
