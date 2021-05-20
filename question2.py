645cd0f448f2589b292cb9594465b657dda9974a
rosickyli <32864696+rosickyli@users.noreply.github.com>
2021-05-20 10:19:34 +0800

    Update question2.py

--- question2.py
+++ question2.py
@@ -19,7 +19,7 @@ import pandas as pd
 import numpy as np
 
 df = pd.DataFrame(pd.read_csv('C:/Users/admin/Desktop/playerstatus.csv'))
-df["date_time"] = pd.to_datetime(df['date_time']).apply(lambda x: x.replace(tzinfo=None))
+df["date_time"] = pd.to_datetime(df['date_time']).apply(lambda x: x.replace(tzinfo=None).strftime('%Y-%m-%d  %H:%M:%S'))
 df = df.fillna('0')
 df.loc[df['skill_left'] <= df['skill_right'], 'skill_left'] = df['skill_right']
 df.loc[df['skill_left'] <= df['skill_right'], 'skill_right'] = df['skill_left']
@@ -28,4 +28,4 @@ df['skill'] = df['skill'].replace('[^a-z]',' ',regex=True).str.rstrip().replace(
 df['date_max'] = df['date_time']
 df = df.groupby(['skill']).agg({'date_time':'min','date_max':'max','player_id': pd.Series.nunique})
 df.rename(columns={'date_time':'date_min','player_id':'number_of_unique_players'}, inplace = True)
-df.to_csv('C:/Users/admin/Desktop/question2.csv')
+df.to_csv('C:/Users/admin/Desktop/agg_skill_combo.csv')
